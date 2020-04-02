2c8b9f561a07d3aa7faf83f5bacd6aaa745b6092
lucetre <lucetre@naver.com>
2020-04-02 21:12:24 +0900

    End of Week 3

--- Week 3/index.js
+++ Week 3/index.js
@@ -1,1 +1,27 @@
-alert('hello world2');
+// alert('hello world2');
+
+
+const sum = (n) => {
+    result = 0;
+    for (let i = 1; i <= n; i++) {
+        result += i;
+    }
+    return result;
+};
+
+console.log(sum(10));
+
+
+const printStars = (n) => {
+    result = '';
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < i; j++)
+            result += ' ';
+        for (let j = n-i; j > 0; j--)
+            result += '*';
+        result += '\n';
+    }
+    console.log(result);
+};
+
+printStars(5);
